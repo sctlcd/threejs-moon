e2ec982c7d55bae6635218cce0011e52dd511603
lucinda <lucinda.souchet@gmail.com>
2022-09-30 16:22:16 +0200

    test deployment

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+// export default {
+//   base: 'http://localhost:8080/'
+// }
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: 'https://sctlcd.github.io/three-js-moon/'
+});
