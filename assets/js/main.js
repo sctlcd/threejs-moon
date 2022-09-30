49f82f73e0bbac0a2b4a31aabe409275418c546b
lucinda <lucinda.souchet@gmail.com>
2022-09-30 19:37:06 +0200

    three import update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 import '../style/style.css';
 
 // import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.144.0/three.module.js';
-import * as THREE from 'https://unpkg.com/three@0.144.0/build/three.module.js';
-// import * as THREE from '/node_modules/three/build/three.module.js';
+// import * as THREE from 'https://unpkg.com/three@0.144.0/build/three.module.js';
+import * as THREE from '/node_modules/three/build/three.module.js';
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls';
 import { OrbitControls } from 'https://unpkg.com/three@0.144.0/examples/jsm/controls/OrbitControls.js';
